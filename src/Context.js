c4488c74cfba292d29bd40c83e787e5a5be53fe3
treelite <c.xinle@gmail.com>
2015-02-04 14:27:37 +0800

    Implement client

---
+++ src/Context.js
@@ -0,0 +1,21 @@
+/**
+ * @file 请求执行环境(Client)
+ * @author treelite(c.xinle@gmail.com)
+ */
+
+define(function (require) {
+
+    var inherits = require('saber-lang').inherits;
+    var Abstract = require('./AbstractContext');
+    var Emitter = require('saber-emitter');
+
+    function Context(Requester) {
+        Abstract.call(this, Requester);
+        // 添加事件机制
+        Emitter.mixin(this);
+    }
+
+    inheriits(Context, Abstract);
+
+    return Context;
+});
