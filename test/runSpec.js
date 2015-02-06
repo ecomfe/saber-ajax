76d94fa3d954b4bcd2b7c2ae8698860be25e790a
treelite <c.xinle@gmail.com>
2015-02-06 13:51:50 +0800

    Update test spec

---
+++ test/runSpec.js
@@ -0,0 +1,29 @@
+/**
+ * @file Run test for Node
+ * @author treelite(c.xinle@gmail.com)
+ */
+
+var loader = require('amder');
+
+var ajax = require('../main');
+var ejson = require('../main').ejson;
+
+var options = {
+    host: 'http://local:8848'
+};
+
+ajax.config(options);
+ejson.config(options);
+
+loader.config({
+    packages: [
+        {
+            name: 'saber-ajax',
+            location: require.resolve('../')
+        }
+    ]
+});
+
+require('./spec/ajax');
+
+require('./spec/ejson');
